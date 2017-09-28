96b8d85ab5a5c66833e68271bcfd02422e87bc44
Cz314 <cz314@hotmail.com>
2017-09-28 18:33:59 +0800

    MyRepository
    githubTest

---
+++ githubTest/githubTest/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  githubTest
+//
+//  Created by Cz on 2017/9/28.
+//  Copyright © 2017年 Cz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
